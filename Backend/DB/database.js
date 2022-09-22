8a70547c16a20863a198177e8558da4e5ca9f74e
Ritu Kumar <95040772+Rupakrajput19@users.noreply.github.com>
2022-09-22 10:41:11 +0530

    Delete Backend directory

--- Backend/DB/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-mongoose.connect("mongodb://localhost:27017/SinUp_User_DB")
-
-.then(() => console.log("Connection Successfull with DB..."))
-
-.catch((error) => console.log(error));
--- Backend/index.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-
-const PORT = 2022;
-
-app.listen((PORT), () => {
-    console.log(`App is running at ${PORT}`);
-});
